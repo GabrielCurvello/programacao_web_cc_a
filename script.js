2ce4615d68fd1a6f66dd560917b661d775de0dc9
GabrielCurvello <gcurvello2l@gmail.com>
2024-08-12 10:48:23 -0300

    script js

---
+++ script.js
@@ -0,0 +1,11 @@
+let elementoemail = document.getElementById("email").value; 
+let entrar = document.getElementById("entrar");
+
+entrar.addEventListener("click", submeter);
+
+function submeter() {
+    let elementoemail = document.getElementById("email").value;
+    let elementosenha = documento.getElementById("senha").value;
+    console.log(elementoemail);
+    console.log(elementosenha);
+}
